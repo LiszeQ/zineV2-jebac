d3698184319ed97e782be1b148c834ba0e7d9b94
Aleks <62609477+Aleks-1123@users.noreply.github.com>
2020-12-30 21:02:37 +0100

    Update kick.js

--- cmds/mod/kick.js
+++ cmds/mod/kick.js
@@ -14,7 +14,9 @@ module.exports.run = async (message, args, client) => {
         try { user.kick(reason) } catch { return client.embeds.error(message, client.lang.handle(message, 'I can\'t kick this user', 'Nie mogę wyrzucić tego użytkownika')) }
 
         // INFRACTIONS
-
+// Created by lambda v1, by sebt08 and Aleks1123
+// Sador to podjebal od nas lmao
+// Kod jest slaby, stary ale nadal nasz
         client.infractions.ensure(`${user.id}_on_${message.guild.id}`, { history: [] })
         const sch = {
             action: 'Kick',
