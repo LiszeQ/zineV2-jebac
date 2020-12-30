3ed98a304d8c7f1071193541370ed0752701b5e4
Aleks <62609477+Aleks-1123@users.noreply.github.com>
2020-12-30 21:03:10 +0100

    Update deletecase.js

--- cmds/mod/deletecase.js
+++ cmds/mod/deletecase.js
@@ -5,6 +5,9 @@ module.exports.run = async (message, args, client) => {
     else if (!client.infractions.get(`${num}_on_${message.guild.id}`)) return client.embeds.error(message, client.lang.handle(message, 'This case does not exist', 'Ten case nie istnieje na tym serwerze'))
     else if (client.infractions.get(`${num}_on_${message.guild.id}`, 'deleted')) return client.embeds.error(message, client.lang.handle(message, 'This case does not exist', 'Ten case nie istnieje na tym serwerze'))
     else {
+        // Created by lambda v1, by sebt08 and Aleks1123
+// Sador to podjebal od nas lmao
+// Kod jest slaby, stary ale nadal nasz
         client.infractions.set(`${num}_on_${message.guild.id}`, true, 'deleted')
         client.base.set(message.guild.id, Math.floor(parseInt(client.base.get(message.guild.id, 'totalCases')) + 1), 'totalCases')
         client.embeds.successWithTick(message, client.lang.handle(message, 'Deleted case', 'Usunięto case'), client.lang.handle(message, `Successfully deleted case **#${num}**.`, `Pomyślnie usunięto case **#${num}**.`))
